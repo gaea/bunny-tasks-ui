5d6b70d15f13dec41c890ab16140431cbbee1650
gustavo espinosa <gaea.uv@gmail.com>
2020-05-25 23:48:09 -0500

    master: added settings to deploy on heroku

--- src/helpers/ApiClient.js
+++ src/helpers/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: 'https://bunny-tasks.herokuapp.com/api',
   withCredentials: false,
   headers: {
     Accept: 'application/json'
